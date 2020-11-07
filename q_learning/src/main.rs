e51f6c6584a26fb037ae875e1f1ab30f5e7b3fb4
DenTaku <dentaku1412@gmail.com>
2020-11-07 16:02:25 +0900

    Design roughly

--- q_learning/src/main.rs
+++ q_learning/src/main.rs
@@ -1,3 +1,56 @@
+#![allow(dead_code)]
+
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+}
+
+// position
+struct Coordinate2d {
+    x: i8,
+    y: i8,
+}
+
+struct QValue {
+    value: [f32; 36], // this time 6x6
+    reinforcement_signal: Vec<[i8; 4]>
+}
+
+// decide next action with ε-Greedy
+struct EpsironGreedy {
+    epsiron: f32,
+    q_value: Rc<QValue>
+}
+
+impl DecideAction for EpsironGreedy {
+    fn decide_action(&self, now_position: Coordinate2d) -> Coordinate2d{
+        unimplemented!()
+    }
+}
+
+// decide next action using ε-Greedy, softmax, and so-on.
+trait DecideAction {
+    fn decide_action(&self, now_position: Coordinate2d) -> Coordinate2d;
+}
+
+// decide state depending on subject
+trait DecideState {
+    fn decide_state(&self, now_position: Coordinate2d, next_position: Coordinate2d) -> Result<Coordinate2d, usize>;
+}
+
+fn q_search_goal(
+    start: Coordinate2d,
+    goal: Coordinate2d,
+    action_determiner: Rc<DecideAction>,
+    state_determiner: Rc<DecideState>
+) {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        assert_eq!(1 + 2, 3)
+    }
 }
