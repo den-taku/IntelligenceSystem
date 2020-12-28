27a50afecdbeb483f5abda03dad0950ce4787fc7
DenTaku <dentaku1412@gmail.com>
2020-12-29 03:37:14 +0900

    Refactoring

--- em_algorithm/src/main.rs
+++ em_algorithm/src/main.rs
@@ -4,7 +4,22 @@ use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 
 fn main() {
-    let file = File::open("mnist_em.csv").expect("fail to open mnist_em.csv");
+    // read data from mnist_em.csv, which has 21770 handwritten characters' images that consists of 28x28 pixels.
+    let mut data = read_csv::<f64>("mnist_em.csv");
+    assert!(data.iter().all(|m| m.len() == 28 * 28));
+    assert_eq!(data.len(), 21770);
+
+    // normalize data
+    data = data.iter().map(|e| e / 255f64).collect();
+    assert!(data.iter().all(|m| m.len() == 28 * 28));
+    assert_eq!(data.len(), 21770);
+}
+
+fn read_csv<T>(filename: &str) -> Vec<Matrix<T>>
+where
+    T: Clone + FromStr,
+{
+    let file = File::open(filename).expect(&format!("fail to open {}", filename));
     let reader = BufReader::new(file);
 
     let mut data = Vec::new();
@@ -15,11 +30,18 @@ fn main() {
             continue;
         }
         let line = line.unwrap();
-        let v: Vec<f64> = line.split(',').map(|s| f64::from_str(s).unwrap()).collect();
+        let v: Vec<T> = line
+            .split(',')
+            .map(|s| T::from_str(s))
+            .map(|r| {
+                if let Ok(e) = r {
+                    e
+                } else {
+                    panic!("fail to change type.")
+                }
+            })
+            .collect();
         data.push(Matrix::append(28, 28, v));
     }
-    
-    assert!(data.iter().all(|m| m.len() == 28 * 28));
-    assert_eq!(data.len(), 21770);
-    // println!("{:?}", data);
+    data
 }
