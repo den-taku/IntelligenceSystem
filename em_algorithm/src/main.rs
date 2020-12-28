e0c6e6147b53a655a61255d15a257e5d7aebc9b6
DenTaku <dentaku1412@gmail.com>
2020-12-29 02:58:00 +0900

    Manage modules

--- em_algorithm/src/main.rs
+++ em_algorithm/src/main.rs
@@ -1,17 +1,14 @@
-mod matrix;
-
+use em_algorithm::matrix::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
-
-use matrix::*;
 
 fn main() {
     let file = File::open("mnist_em.csv").expect("fail to open mnist_em.csv");
     let reader = BufReader::new(file);
 
     let mut data = Vec::new();
-    
+
     for (index, line) in reader.lines().enumerate() {
         if index == 0 {
             continue;
