bc177f180f922a924e51a8c46ab3d21fb6abd913
DenTaku <dentaku1412@gmail.com>
2020-12-28 23:40:07 +0900

    Prepare for opening files

--- em_algorithm/src/main.rs
+++ em_algorithm/src/main.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+
 fn main() {
+    let mut file = File::open("mnist_em.csv").expect("fail to open mnist_em.csv");
+    let mut file_for_input = File::create("src/input.rs").expect("fail to open input.rs");
     println!("Hello, world!");
 }
