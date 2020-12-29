a766593225cbb04febc9a79d9ff3e46e04d85c8b
DenTaku <dentaku1412@gmail.com>
2020-12-30 00:58:46 +0900

    Design

--- k_means/src/main.rs
+++ k_means/src/main.rs
@@ -1,4 +1,5 @@
 use k_means::data_manage::*;
+use k_means::k_means::*;
 
 fn main() {
     // read data
@@ -12,4 +13,9 @@ fn main() {
 
     // define mixed number
     let mixed_number = 3;
+
+    let mut k_means = KMeans::new(mixed_number, training_data);
+
+    let parameters = k_means.estimate();
+    println!("{:?}", parameters);
 }
