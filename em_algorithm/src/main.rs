dc0a4b0bdbeee038de10e26ef1afb2eed483b793
DenTaku <dentaku1412@gmail.com>
2020-12-29 17:03:02 +0900

    Remove assertion

--- em_algorithm/src/main.rs
+++ em_algorithm/src/main.rs
@@ -1,24 +1,21 @@
 use em_algorithm::matrix::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::str::FromStr;
 use std::ops::Div;
+use std::str::FromStr;
 
 fn main() {
-    // read data from mnist_em.csv, which has 21770 handwritten characters' images that consists of 28x28 pixels.
+    // read data from mnist_em.csv, which has
+    // 21770 handwritten characters' images that consists of 28x28 pixels.
     let data = read_csv::<f64>("data/mnist_em.csv");
-    assert!(data.iter().all(|m| m.len() == 28 * 28));
-    assert_eq!(data.len(), 21770);
 
     // normalize data
     let data = normalize_data(data, 255f64);
-    assert!(data.iter().all(|m| m.len() == 28 * 28));
-    assert_eq!(data.len(), 21770);
 }
 
 fn normalize_data<T>(data: Vec<Matrix<T>>, norm: T) -> Vec<Matrix<T>>
-where 
-    T: Div<Output = T> + Clone + Copy
+where
+    T: Div<Output = T> + Clone + Copy,
 {
     let data: Vec<Matrix<T>> = data.iter().map(|e| e / norm).collect();
     data
