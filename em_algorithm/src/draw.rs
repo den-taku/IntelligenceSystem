74ab3cf2bfbf0dcb7426c9185eb0e1d54399e15a
DenTaku <dentaku1412@gmail.com>
2021-01-02 03:21:25 +0900

    Implement draw_graph

--- em_algorithm/src/draw.rs
+++ em_algorithm/src/draw.rs
@@ -24,7 +24,33 @@ pub fn write_image<F: Clone + FromPrimitive + Float>(
     Ok(())
 }
 
-pub fn draw_graph_log(
+pub fn draw_graph(
+    x_range_from: f64,
+    x_range_to: f64,
+    y_range_from: f64,
+    y_range_to: f64,
+    x_label: &str,
+    y_label: &str,
+    color: &str,
+    data: Vec<(f64, f64)>,
+) {
+    let mut fg = Figure::new();
+    {
+        let axec = fg
+            .axes2d()
+            .set_x_axis(true, &[])
+            .set_x_range(Fix(x_range_from), Fix(x_range_to))
+            .set_y_range(Fix(y_range_from), Fix(y_range_to))
+            .set_x_label(x_label, &[])
+            .set_y_label(y_label, &[]);
+        data.iter().fold((), |_, e| {
+            axec.points(&[e.0], &[e.1], &[Color(color), PointSymbol('O')]);
+        })
+    }
+    let _ = fg.show();
+}
+
+pub fn draw_graph_log10(
     x_range_from: f64,
     x_range_to: f64,
     y_range_from: f64,
